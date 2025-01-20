59267792c3eabf93204c3af5b38abce9378ad3b1
Nicholas Furgiuele <nicholas@distinctive.com.au>
2025-01-20 21:40:04 +1100

    Add keyboard shortcut to change pattern index.

--- wwwroot/modules/main.js
+++ wwwroot/modules/main.js
@@ -318,6 +318,8 @@ const keyboardCommands = {
     paletteIndexHigher: 'paletteIndexHigher',
     paletteIndexLower: 'paletteIndexLower',
     paletteSwapPrimarySecondary: 'paletteSwapPrimarySecondary',
+    patternIndexHigher: 'patternIndexHigher',
+    patternIndexLower: 'patternIndexLower',
     tileNew: 'tileNew',
     tileDelete: 'tileDelete',
     tileMapNew: 'tileMapNew',
@@ -441,6 +443,12 @@ function createEventListeners() {
     ]));
     keyboardManager.addKeyHandler(new KeyDownHandler(keyboardCommands.tileMirrorVertical, [
         { modifiers: { alt: true }, key: [']', '}', '‘'] }
+    ]));
+    keyboardManager.addKeyHandler(new KeyDownHandler(keyboardCommands.patternIndexLower, [
+        { modifiers: { shift: true }, key: ['[', '{', '“'] }
+    ]));
+    keyboardManager.addKeyHandler(new KeyDownHandler(keyboardCommands.patternIndexHigher, [
+        { modifiers: { shift: true }, key: [']', '}', '‘'] }
     ]));
     keyboardManager.addKeyHandler(new KeyDownHandler(keyboardCommands.tileMapNew, [
         { modifiers: { alt: true }, keySeries: [{ code: ['n', 'N'] }, { key: ['m', 'M'] }] },
@@ -659,6 +667,12 @@ function createEventListeners() {
                         updateTilesOnEditors([tile.tileId]);
                     }
                 }
+                break;
+            case keyboardCommands.patternIndexLower:
+                patternIndexHigherOrLower('LOWER');
+                break;
+            case keyboardCommands.patternIndexHigher:
+                patternIndexHigherOrLower('HIGHER');
                 break;
             case keyboardCommands.moveUp:
                 if (isTileSet()) {
@@ -1648,7 +1662,7 @@ function handleTileEditorOnEvent(args) {
                     isInBounds: args.isInBounds,
                     controlKey: args.ctrlKeyPressed,
                     shiftKey: args.shiftKeyPressed,
-                event: TileEditor.Events.pixelMouseUp
+                    event: TileEditor.Events.pixelMouseUp
                 });
                 instanceState.operationTileIndex = -1;
                 if (instanceState.undoDisabled) {
@@ -2895,17 +2909,17 @@ function takeToolAction(args) {
 
                         const updatedTiles = PaintTool.paintOntoTileGrid(getTileGrid(), getTileSet(), {
                             coordinate: { x: imageX, y: imageY },
-                            brush: { 
-                                primaryColourIndex: instanceState.colourIndex, 
-                                secondaryColourIndex: instanceState.secondaryColourIndex, 
-                                size: instanceState.pencilSize 
+                            brush: {
+                                primaryColourIndex: instanceState.colourIndex,
+                                secondaryColourIndex: instanceState.secondaryColourIndex,
+                                size: instanceState.pencilSize
                             },
-                            options: { 
-                                constrainToColourIndex: (tool === TileEditorToolbar.Tools.colourReplace) ? instanceState.startingColourIndex : null, 
+                            options: {
+                                constrainToColourIndex: (tool === TileEditorToolbar.Tools.colourReplace) ? instanceState.startingColourIndex : null,
                                 clampToTile: clamp
                             },
                             pattern: {
-                                pattern: (instanceState.patternIndex > -1) ? patternManager.getPattern(instanceState.patternIndex) : null, 
+                                pattern: (instanceState.patternIndex > -1) ? patternManager.getPattern(instanceState.patternIndex) : null,
                                 originX: instanceState.patternOriginX,
                                 originY: instanceState.patternOriginY
                             }
@@ -3600,6 +3614,29 @@ function setPatternIndex(value) {
     tileContextToolbar.setState({
         patternIndex: instanceState.patternIndex
     });
+}
+
+/**
+ * Sets the pattern index to the previous or next one.
+ * @param {'LOWER'|'HIGHER'} direction - Make it lower or higher.
+ */
+function patternIndexHigherOrLower(direction) {
+    const patternCount = patternManager.getAllPatterns().length;
+    let updatedIndex = instanceState.patternIndex;
+
+    if (direction === 'HIGHER') {
+        updatedIndex++;
+    } else if (direction === 'LOWER') {
+        updatedIndex--;
+    }
+
+    if (updatedIndex < -1) {
+        updatedIndex = patternCount - 1;
+    } else if (updatedIndex >= patternCount) {
+        updatedIndex = -1;
+    }
+
+    setPatternIndex(updatedIndex);
 }
 
 /**
